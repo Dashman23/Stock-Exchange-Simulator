8ea5847b9909ac845722c29d859e24cbf577aba9
Anthony <90662087+AnthonyF144@users.noreply.github.com>
2023-04-14 00:10:09 -0400

    made some changes to websocket, and made some changes to the html

--- FinalAssignment/src/main/webapp/js/main.js
+++ FinalAssignment/src/main/webapp/js/main.js
@@ -1,69 +1,102 @@
 var chart;
-		var interval;
+var interval;
 
-		function startChart() {
-			var ctx = document.getElementById('chart').getContext('2d');
-			chart = new Chart(ctx, {
-				type: 'line',
-				data: {
-					labels: [],
-					datasets: [{
-						label: 'Stock Price',
-						data: [],
-						borderColor: 'rgb(255, 99, 132)',
-						fill: false
-					}]
-				},
-				options: {
-					responsive: true,
-					animation: false,
-					scales: {
-						xAxes: [{
-							display: true,
-							ticks: {
-								maxTicksLimit: 10
-							}
-						}],
-						yAxes: [{
-							display: true,
-							ticks: {
-								beginAtZero: true
-							}
-						}]
+function startChart() {
+	var ctx = document.getElementById('chart').getContext('2d');
+	chart = new Chart(ctx, {
+		type: 'line',
+		data: {
+			labels: [],
+			datasets: [{
+				label: 'Stock Price',
+				data: [],
+				borderColor: 'rgb(255, 99, 132)',
+				fill: false
+			}]
+		},
+		options: {
+			responsive: true,
+			animation: false,
+			scales: {
+				xAxes: [{
+					display: true,
+					ticks: {
+						maxTicksLimit: 10
 					}
-				}
-			});
+				}],
+				yAxes: [{
+					display: true,
+					ticks: {
+						beginAtZero: true
+					}
+				}]
+			}
+		}
+	});
+	var socket = new WebSocket("ws://localhost:8080/FinalAssignment-1.0-SNAPSHOT/ws/stocks");
+    socket.onmessage = function(event) {
+        var stockData = JSON.parse(event.data);
+        var stockPrice = stockData.price;
+        var time = new Date(stockData.time).toLocaleTimeString();
+        chart.data.labels.push(time);
+        chart.data.datasets[0].data.push(stockPrice);
+        if (chart.data.labels.length > 10) {
+            chart.data.labels.shift();
+            chart.data.datasets[0].data.shift();
+        }
+        chart.update();
+    };
 
-			interval = setInterval(function() {
-				// Replace the below code with your actual stock data API
-				if (ws != null) {
-					ws.close();
-				}
-
-				// create the websocket
-				ws = new WebSocket("http://localhost:8080/FinalAssignment-1.0-SNAPSHOT/ws/stocks");
-
-				ws.onmessage = function (event) {
-					console.log(event.data);
-					// parsing the server's message as json
-					let message = JSON.parse(event.data);
-					document.getElementById("log").value += "(" + timestamp() + ") " + message.message + "\n";
-				}
-
-
-
-				var stockPrice = Math.floor(Math.random() * 100) + 1;
-				var time = new Date().toLocaleTimeString();
-				chart.data.labels.push(time);
-				chart.data.datasets[0].data.push(stockPrice);
-				if (chart.data.labels.length > 10) {
-					chart.data.labels.shift();
-					chart.data.datasets[0].data.shift();
-				}
-				chart.update();
-			}, 100);
+    interval = setInterval(function() {
+        socket.send("request");
+    }, 5000);
+	/* A json like this will be returned
+	{
+		"symbol": "AAPL",
+		"price": 145.63,
+		"time": "2023-04-14T01:23:45.678Z"
+	}
+	*/
+	/*interval = setInterval(function() {
+		// Replace the below code with your actual stock data API
+		if (ws != null) {
+			ws.close();
 		}
 
-		function stopChart() {
-			clearInterval(interval);
+		// create the websocket
+		ws = new WebSocket("http://localhost:8080/FinalAssignment-1.0-SNAPSHOT/ws/stocks");
+
+		ws.onmessage = function (event) {
+			console.log(event.data);
+			// parsing the server's message as json
+			let message = JSON.parse(event.data);
+			document.getElementById("log").value += "(" + timestamp() + ") " + message.message + "\n";
 		}
+
+		var stockPrice = Math.floor(Math.random() * 100) + 1;
+		var time = new Date().toLocaleTimeString();
+		chart.data.labels.push(time);
+		chart.data.datasets[0].data.push(stockPrice);
+		if (chart.data.labels.length > 10) {
+			chart.data.labels.shift();
+			chart.data.datasets[0].data.shift();
+		}
+		chart.update();
+	}, 100);*/
+}
+
+function stopChart() {
+	clearInterval(interval);
+}
+
+function sendOrder(){
+		
+}
+/* {
+        "symbol": "AAPL",
+        "quantity": 145.63,
+    }
+	*/
+function buyOrder(){
+	
+}
